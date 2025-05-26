3df0427b570ea2bd361d62fb805830c1275e3f2d
LorenzoCalabrese03 <151526103+LorenzoCalabrese03@users.noreply.github.com>
2025-05-26 11:42:10 +0200

    uso di pytest

--- tests/test_esempio.py
+++ tests/test_esempio.py
@@ -1,9 +1,4 @@
-import unittest
 from src.esempio import somma
 
-class TestSomma(unittest.TestCase):
-    def test_somma(self):
-        self.assertEqual(somma(2, 3), 5)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_somma():
+    assert somma(2, 3) == 5
