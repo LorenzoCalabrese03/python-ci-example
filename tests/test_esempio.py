cf38c06b0d349ca5a2ecdaea702e017227b59777
LorenzoCalabrese03 <151526103+LorenzoCalabrese03@users.noreply.github.com>
2025-05-26 11:29:57 +0200

    update

--- tests/test_esempio.py
+++ tests/test_esempio.py
@@ -1,11 +1,5 @@
 import unittest
-import sys
-import os
-
-# Aggiunge 'src' al path per permettere gli import
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-
-from esempio import somma
+from src.esempio import somma
 
 class TestSomma(unittest.TestCase):
     def test_somma(self):
